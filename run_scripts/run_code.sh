fbff0d9dbea2403dbbe9eb6d7968e7e55ed9275e
Benjamin-Walker <ben.walker@ladeck.com>
2023-08-23 14:23:30 +0100

    Allowed specifiyng data root

--- run_scripts/run_code.sh
+++ run_scripts/run_code.sh
@@ -18,15 +18,15 @@ which python
 
 # Define and run experiment
 root=/data/inet-multimodal-ai/wolf6245/data
-experiment_name=torch_-24htorch_-12h_voting
+experiment_name=rocket_-6h_rf_noagg_full
 split_column=split
 
 split=1
 python move_test_files_back.py $root
 python move_test_files_out.py $split $split_column $root
-python train_model.py "/data/inet-multimodal-ai/wolf6245/data/physionet_challenge_2023/physionet.org/files/i-care/2.0/training/" "data/02_models/${experiment_name}/split_${split_column}_${split}/"
-python run_model.py "data/02_models/${experiment_name}/split_${split_column}_${split}/" "/data/inet-multimodal-ai/wolf6245/data/physionet_challenge_2023/physionet.org/files/i-care/2.0/testing/" "data/03_model_output/${experiment_name}/split_${split_column}_${split}/"
-python evaluate_model.py "/data/inet-multimodal-ai/wolf6245/data/physionet_challenge_2023/physionet.org/files/i-care/2.0/testing/" "data/03_model_output/${experiment_name}/split_${split_column}_${split}/" "data/04_reportings/${experiment_name}/split_${split_column}_${split}_results.csv"
+python train_model.py "${root}/physionet_challenge_2023/physionet.org/files/i-care/2.0/training/" "data/02_models/${experiment_name}/split_${split_column}_${split}/"
+python run_model.py "data/02_models/${experiment_name}/split_${split_column}_${split}/" "${root}/physionet_challenge_2023/physionet.org/files/i-care/2.0/testing/" "data/03_model_output/${experiment_name}/split_${split_column}_${split}/"
+python evaluate_model.py "${root}/physionet_challenge_2023/physionet.org/files/i-care/2.0/testing/" "data/03_model_output/${experiment_name}/split_${split_column}_${split}/" "data/04_reportings/${experiment_name}/split_${split_column}_${split}_results.csv"
 python move_test_files_back.py $root
 
 # split=2
